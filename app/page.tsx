53fb1fc796e94956451c89d3ea3634d380cea662
omeatai <omeatai@gmail.com>
2023-11-09 11:33:22 -0700

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
                   <path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zM349.3 793.7H230.6V411.9h118.7v381.8zm-59.3-434a68.8 68.8 0 1168.8-68.8c-.1 38-30.9 68.8-68.8 68.8zm503.7 434H675.1V608c0-44.3-.8-101.2-61.7-101.2-61.7 0-71.2 48.2-71.2 98v188.9H423.7V411.9h113.8v52.2h1.6c15.8-30 54.5-61.7 112.3-61.7 120.2 0 142.3 79.1 142.3 181.9v209.4z" />
                 </svg>
               </a>
-              <a href="omeatai@gmail.com" target="_blank">
+              <a href="" target="_blank">
                 <svg
                   viewBox="0 0 1024 1024"
                   fill="currentColor"
@@ -53,7 +53,7 @@ export default function Home() {
                   <path d="M928 160H96c-17.7 0-32 14.3-32 32v640c0 17.7 14.3 32 32 32h832c17.7 0 32-14.3 32-32V192c0-17.7-14.3-32-32-32zm-80.8 108.9L531.7 514.4c-7.8 6.1-18.7 6.1-26.5 0L189.6 268.9A7.2 7.2 0 01194 256h648.8a7.2 7.2 0 014.4 12.9z" />
                 </svg>
               </a>
-              <a href="" target="_blank">
+              <a href="https://twitter.com/iomeata" target="_blank">
                 <svg
                   viewBox="0 0 1024 1024"
                   fill="currentColor"
